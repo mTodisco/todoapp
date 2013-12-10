df0b7aa67b69d35a1a3fe184465aa2ffac62c6a0
mTodisco <mtodisco@hwr-berlin.de>
2013-12-10 16:15:08 +0100

    Username zu User hinzugefügt

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131210141007) do
+ActiveRecord::Schema.define(version: 20131210151130) do
 
   create_table "tasks", force: true do |t|
     t.string   "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20131210141007) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
